c8c16d26e7e9c390a7aa94591a2a2a7fd986e2a5
karthikk00 <60411071+karthikk00@users.noreply.github.com>
2022-04-12 19:00:53 -0400

    Update createPowerLines.cs

--- Assets/createPowerLines.cs
+++ Assets/createPowerLines.cs
@@ -1680,6 +1680,26 @@ public class createPowerLines : MonoBehaviour
         
 
     }
+    // bool PowerLineTerrainLevel(int playerTurn, float x, float y)
+    // {
+    //     GameObject temp = new GameObject();
+    //     if (SetColor(temp) == Color.green)
+    //     {
+    //         //deleted so code can still run
+    //     }
+    //     else if (SetColor(temp) = Color.blue)
+    //     {
+
+    //     }
+    //     else if (SetColor(temp) == Color.gray)
+    //     {
+
+    //     }
+    //     else if (SetColor(temp) = Color.yellow)
+    //     {
+            
+    //     }
+    // }
     bool checkMaxPowerLines(int playerTurn, float x, float y)
     {
         for (int i = 0; i < allPowLineSpots.Count; i++)
